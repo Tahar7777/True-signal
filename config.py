1bf895e22393aa8b0d9d589ed0e5ed95ee21d961
Tahar7777 <taharotmane7@gmail.com>
2025-05-23 20:41:21 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
-
 CONFIG = {
     'FLASK_SECRET_KEY': 'your-secret-key',
-    'BYBIT_API_URL': 'https://api.bybit.com/v5/market/tickers?category=linear',
-    'SCAN_INTERVAL': 300,  # 5 دقائق
-    'MAX_SIGNALS': 10      # أقصى عدد للإشارات المعروضة
+    'TICKERS_URL': 'https://api.bybit.com/v5/market/tickers?category=linear',
+    'SCAN_INTERVAL': 60,  # كل دقيقة
+    'MAX_SIGNALS': 5
 }
